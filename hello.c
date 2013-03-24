cf2d4f56d81d711a576cb487577db04b81293394
zhangxinjian <zhangxinjian1212@gmail.com>
2013-03-24 17:00:04 +0800

    modife hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 //#include <>
+int print(void);
 
 int main(void)
 {
     printf("Hello world!!\n");
+    print();
     return 0;
 }
 
+int print(void)
+{
+    printf("Hello world ha ha!!\n");
+    return 0;
+}
